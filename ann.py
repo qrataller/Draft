57d10acbba1e3318f5afec3e3c1c27902dff99f0
Rose Ann Taller <132386238+qrataller@users.noreply.github.com>
2024-05-18 19:16:39 +0800

    Add files via upload

---
+++ ann.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 18 14:00:25 2024
+
+@author: Roseann
+"""
+
+import cv2
+import streamlit as st
+import tensorflow as tf
+from PIL import Image, ImageOps
+import numpy as np
+import tensorflow_hub as hub
+
+model_path = "dog1.h5"
+
+@st.cache_resource
+def load_model(model_path):
+    """
+    Loads a saved model from a specified path.
+    """
+    print(f"Loading saved model from: {model_path}")
+    custom_objects = {"KerasLayer": hub.KerasLayer}
+    model = tf.keras.models.load_model(model_path, custom_objects=custom_objects)
+    print("Model loaded successfully.")
+    print("Model summary:")
+    print(model.summary())  # Print model summary to verify its architecture
+    return model
+
+st.write("""
+# Dog Classification by
+## Gonzales and Taller"""
+)
+file=st.file_uploader("Choose photo of a dog from your computer",type=["jpg","png"])
+
+
+
+def import_and_predict(image_data, model):
+    size = (128, 128)
+    
+    # Resize the image to the expected input shape of the model
+    image = ImageOps.fit(image_data, size, Image.LANCZOS)
+    img = np.asarray(image)
+    img = cv2.resize(img, (128, 128), interpolation=cv2.INTER_NEAREST)
+    
+    # Convert the image to grayscale if necessary
+    if img.ndim == 3 and img.shape[2] == 3:
+        img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+
+    # Reshape the image to add a channel dimension
+    img_reshape = img.reshape((1,) + img.shape + (1,))
+
+    # Make predictions using the Keras model
+    prediction = model.predict(img_reshape)
+    return prediction
+
+
+if file is None:
+    st.text("Please upload an image file.")
+
+else:
+    loaded_model = load_model(model_path) 
+    image = Image.open(file)
+    st.image(image, use_column_width=True)
+    prediction = import_and_predict(image,loaded_model)
+    class_names=['affenpinscher', 'afghan_hound', 'african_hunting_dog', 'airedale',
+                         'american_staffordshire_terrier', 'appenzeller',
+                         'australian_terrier', 'basenji', 'basset', 'beagle',
+                         'bedlington_terrier', 'bernese_mountain_dog',
+                         'black-and-tan_coonhound', 'blenheim_spaniel', 'bloodhound',
+                         'bluetick', 'border_collie', 'border_terrier', 'borzoi',
+                         'boston_bull', 'bouvier_des_flandres', 'boxer',
+                         'brabancon_griffon', 'briard', 'brittany_spaniel', 'bull_mastiff',
+                         'cairn', 'cardigan', 'chesapeake_bay_retriever', 'chihuahua',
+                         'chow', 'clumber', 'cocker_spaniel', 'collie',
+                         'curly-coated_retriever', 'dandie_dinmont', 'dhole', 'dingo',
+                         'doberman', 'english_foxhound', 'english_setter',
+                         'english_springer', 'entlebucher', 'eskimo_dog',
+                         'flat-coated_retriever', 'french_bulldog', 'german_shepherd',
+                         'german_short-haired_pointer', 'giant_schnauzer',
+                         'golden_retriever', 'gordon_setter', 'great_dane',
+                         'great_pyrenees', 'greater_swiss_mountain_dog', 'groenendael',
+                         'ibizan_hound', 'irish_setter', 'irish_terrier',
+                         'irish_water_spaniel', 'irish_wolfhound', 'italian_greyhound',
+                         'japanese_spaniel', 'keeshond', 'kelpie', 'kerry_blue_terrier',
+                         'komondor', 'kuvasz', 'labrador_retriever', 'lakeland_terrier',
+                         'leonberg', 'lhasa', 'malamute', 'malinois', 'maltese_dog',
+                         'mexican_hairless', 'miniature_pinscher', 'miniature_poodle',
+                         'miniature_schnauzer', 'newfoundland', 'norfolk_terrier',
+                         'norwegian_elkhound', 'norwich_terrier', 'old_english_sheepdog',
+                         'otterhound', 'papillon', 'pekinese', 'pembroke', 'pomeranian',
+                         'pug', 'redbone', 'rhodesian_ridgeback', 'rottweiler',
+                         'saint_bernard', 'saluki', 'samoyed', 'schipperke',
+                         'scotch_terrier', 'scottish_deerhound', 'sealyham_terrier',
+                         'shetland_sheepdog', 'shih-tzu', 'siberian_husky', 'silky_terrier',
+                         'soft-coated_wheaten_terrier', 'staffordshire_bullterrier',
+                         'standard_poodle', 'standard_schnauzer', 'sussex_spaniel',
+                         'tibetan_mastiff', 'tibetan_terrier', 'toy_poodle', 'toy_terrier',
+                         'vizsla', 'walker_hound', 'weimaraner', 'welsh_springer_spaniel',
+                         'west_highland_white_terrier', 'whippet',
+                         'wire-haired_fox_terrier', 'yorkshire_terrier']
+    string="OUTPUT : "+ class_names[np.argmax(prediction)]
+    st.success(string)
