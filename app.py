ae109eaef9ef4b018e16eb5c5162e1df3639606b
Rose Ann Taller <132386238+qrataller@users.noreply.github.com>
2024-05-18 23:20:11 +1000

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,6 @@ if file is None:
     st.text("Please upload an image file")
 else:
     image=Image.open(file)
-    st.image(image,use_column_width=True)
     st.image(image, caption="Uploaded image file", use_column_width=True)
     prediction=import_and_predict(image,model)
     st.write("")
